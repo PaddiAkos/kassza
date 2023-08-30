f12cd44716212e77d6add1e3e1368150a1b9c7ac
Akos <akospaddi54@gmail.com>
2023-08-30 12:51:44 +0200

    kjn

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args){
-    int number = 343247;
+        int number = 43247;
 
-    int TwentyThousand = (number / 20000) % 10;
+
     int TenThousand = (number / 10000) % 10;
     int thousands = (number / 1000) % 10;
     int hundreds = (number / 100) % 10;
@@ -24,13 +24,12 @@ public class Main {
         tens += 1;
     }
         System.out.println(number + " Ezt az összeget ilyen bakjegyekkel tudod kifizetni: ");
-        System.out.println("húszezresből: " + TwentyThousand);
         System.out.println("tízezresből: " + TenThousand);
         System.out.println("ezresből: " + thousands);
         System.out.println("százasól: " + hundreds);
         System.out.println("tizesből: " + tens);
         System.out.println("ötforintosból: " + ones);
 
-        
+
 }
     }
